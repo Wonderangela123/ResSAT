d3260dfe8c6fdd40b9c44cd6bd951cd6be3f0e78
Wonderangela123 <140135188+Wonderangela123@users.noreply.github.com>
2026-05-06 09:57:49 -0500

    Update ressat_train.py

--- ressat/ressat_train.py
+++ ressat/ressat_train.py
@@ -7,16 +7,6 @@ import numpy as np
 import random
 import os
 
-
-def set_seed(seed):
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
-    
-set_seed(42)
     
 def main():
     args = parse_args()
